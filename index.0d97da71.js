c8eee08bbde878c31fdb7655c9c0e842f16acb27
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-07-05 14:24:02 +0300

    auto commit [ci skip]

---
+++ index.0d97da71.js
@@ -0,0 +1,2 @@
+!function(e,t,a){(t[a]=t[a]||[]).push((function(){try{t.yaCounter50671996=new Ya.Metrika2({id:50671996,clickmap:!0,trackLinks:!0,accurateTrackBounce:!0})}catch(e){}}));var c=e.getElementsByTagName("script")[0],n=e.createElement("script"),r=function(){c.parentNode.insertBefore(n,c)};n.type="text/javascript",n.async=!0,n.src="https://mc.yandex.ru/metrika/tag.js","[object Opera]"==t.opera?e.addEventListener("DOMContentLoaded",r,!1):r()}(document,window,"yandex_metrika_callbacks2");
+//# sourceMappingURL=index.0d97da71.js.map
